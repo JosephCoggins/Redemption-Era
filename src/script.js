d01078f4246172381c55bb06c361e205f1c6c649
Joseph A. Coggins Jr. <josephcoggins@outlook.com>
2025-08-11 21:06:12 -0500

    Added a header section with collapsible navbar. [ES]

--- src/script.js
+++ src/script.js
@@ -2,15 +2,19 @@ document.addEventListener("DOMContentLoaded", () => {
     const navState = document.querySelector("#nav-state");
     const navLinks = document.querySelector("#nav-links");
     const navToggle = document.querySelector("#nav-toggle");
+    const isEnglish = document.documentElement.lang.startsWith("en") || navigator.language.startsWith("en");
 
     navLinks.setAttribute("aria-expanded", "false");
     navState.addEventListener("change", () => {
+        const collapseMessage = isEnglish ? "Collapse Navigation Menu" : "Colapsar Menú de Navegación";
+        const expandMessage = isEnglish ? "Expand Navigation Menu" : "Expandir Menú de Navegación";
+
         if(navState.checked) {
             navLinks.setAttribute("aria-expanded", "true");
-            navToggle.setAttribute("aria-label", "Collapse Navigation Menu");
+            navToggle.setAttribute("aria-label", collapseMessage);
         } else {
             navLinks.setAttribute("aria-expanded", "false");
-            navToggle.setAttribute("aria-label", "Expand Navigation Menu");
+            navToggle.setAttribute("aria-label", expandMessage);
         }
     });
 });
