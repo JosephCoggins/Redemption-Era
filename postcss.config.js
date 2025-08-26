e18fc6c9add57c529feecffaef3f58662460570b
Joseph A. Coggins Jr. <josephcoggins@outlook.com>
2025-08-26 00:00:05 -0500

    Setup & configured PostCSS.

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+import postcssPresetEnv from "postcss-preset-env";
+
+export default {
+    plugins: [
+        postcssPresetEnv({
+            stage: 3,
+            autoprefixer: {
+                grid: true,
+                remove: true
+            },
+            features: {}
+        })
+    ]
+};
